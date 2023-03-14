629ea5e1dad9fd89401aa6f998b0a0b0244b7651
Ibrahimdata1 <bsk.kosit1@gmail.com>
2023-03-14 19:40:42 +0700

    Create Exercise4_Kosit_B.py

---
+++ Exercise4_Kosit_B.py
@@ -0,0 +1,13 @@
+#Variable
+x=80.0 #Foundation English
+y=72.0 #General Business
+xx=55.0 #Introduction to Computer Systems
+yy=33.0 #Computer PRogramming
+
+#Coding
+print("---Your Score---")
+print("Foundation English  :", x)
+print("General Business  :", y)
+print("Introduction to Computer Systems  :", xx)
+print("Computer PRogramming  :", yy)
+
