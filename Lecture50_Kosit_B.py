821335f3413d267efa651e5b731666707bf480c0
Ibrahimdata1 <bsk.kosit1@gmail.com>
2023-03-16 00:26:45 +0700

    Create Lecture50_Kosit_B.py

---
+++ Lecture50_Kosit_B.py
@@ -0,0 +1,8 @@
+def addNumberPlus(x,y):
+    print("Result =",x+y)
+def addNumberMinus(x,y):
+    print("Result =",x-y)
+def addNumberMultiply(x,y):
+    print("Result =",x*y)
+def addNumberDivide(x,y):
+    print("Result =",x/y)
