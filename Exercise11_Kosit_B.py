67d8c5ab28ed62cb09065aaff5c164c466342a16
Ibrahimdata1 <bsk.kosit1@gmail.com>
2023-03-15 23:37:59 +0700

    Create Exercise11_Kosit_B.py

---
+++ Exercise11_Kosit_B.py
@@ -0,0 +1,8 @@
+number = int(input("Enter number of rows : "))
+for x in range(number):
+    for y in range(number-x-1):
+        print(end=" ")
+    for y in range(x+1):
+        print("*",end=" ")
+    print()
+
