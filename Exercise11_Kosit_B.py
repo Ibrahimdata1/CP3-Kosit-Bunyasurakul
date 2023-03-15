e834d30bdfcd740993074898e4cce820178ea813
Ibrahimdata1 <bsk.kosit1@gmail.com>
2023-03-16 00:35:43 +0700

    Update Exercise11_Kosit_B.py

--- Exercise11_Kosit_B.py
+++ Exercise11_Kosit_B.py
@@ -2,7 +2,7 @@ number = int(input("Enter number of rows : "))
 for x in range(number):
     for y in range(number-x-1):
         print(end=" ")
-    for y in range(x+1):
-        print("*",end=" ")
+    for y in range((2*x)+1):
+        print("*",end="")
     print()
 
